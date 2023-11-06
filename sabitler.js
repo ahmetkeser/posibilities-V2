5a9dd333ea02d46491fd52b1f1f8b1e24116aa19
ahmet keser <ahmetkeser006@gmail.com>
2023-11-06 23:29:54 +0300

    es6 class yapısı uyarlandı

--- sabitler.js
+++ sabitler.js
@@ -1,25 +1,23 @@
 
-function Sabit(){
-  
-    
-}
-Sabit.prototype.clearPanel= function(){
-    const clearDOMConsole = document.getElementById("console-div") // bu kontrol tekrarlanan clicklerde div içersindeki önceki eklenen elemanları temizler (üstüne yazmazını engeller)
-    while (clearDOMConsole.firstChild) {
-        clearDOMConsole.removeChild(clearDOMConsole.firstChild)
+class Sabit{
+    static clearPanel(){
+        const clearDOMConsole = document.getElementById("console-div") // bu kontrol tekrarlanan clicklerde div içersindeki önceki eklenen elemanları temizler (üstüne yazmazını engeller)
+        while (clearDOMConsole.firstChild) {
+            clearDOMConsole.removeChild(clearDOMConsole.firstChild)
+        }
+        const clearDOMImage=document.getElementById("image-div")    // image-div tagını temizle
+        clearDOMImage.removeChild(clearDOMImage.firstChild)
     }
-    const clearDOMImage=document.getElementById("image-div")    // image-div tagını temizle
-    clearDOMImage.removeChild(clearDOMImage.firstChild)
-}
-Sabit.prototype.pushOutput= function(text, textValue = ""){
-        let yapi = document.querySelector("#console-div")
-        let pushOutput = document.createElement("p")
-        pushOutput.textContent = text + textValue
-        yapi.appendChild(pushOutput)
-}
-Sabit.prototype.img=function(img){
-    let yapi2 = document.querySelector("#image-div")
-    let pushOutput2 = document.createElement("img")
-    pushOutput2.src = "image/"+img+".png"
-    yapi2.appendChild(pushOutput2)
+    static pushOutput(text, textValue = ""){
+            let yapi = document.querySelector("#console-div")
+            let pushOutput = document.createElement("p")
+            pushOutput.textContent = text + textValue
+            yapi.appendChild(pushOutput)
+    }
+    static img(img){
+        let yapi2 = document.querySelector("#image-div")
+        let pushOutput2 = document.createElement("img")
+        pushOutput2.src = "image/"+img+".png"
+        yapi2.appendChild(pushOutput2)
+    }
 }
